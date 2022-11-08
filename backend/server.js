08dfd8e184b033052f059de1da792951a7494943
tarikceylan <tarikceylan@outlook.com.tr>
2022-11-08 16:29:24 +0300

    Add express-async-errors

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+
 const PORT = process.env.PORT || 3500;
+
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
