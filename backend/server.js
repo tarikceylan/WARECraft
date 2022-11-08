bc347142961d0f6fee9f47ca50e219c434b9266f
tarikceylan <tarikceylan@outlook.com.tr>
2022-11-08 16:50:20 +0300

    Add mongoose & Establish DB Connection

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,26 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const connectDB = require('./config/dbConn');
 
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 
 const PORT = process.env.PORT || 3500;
 
+connectDB();
+
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT}`);
+mongoose.connection.once('open', () => {
+  console.log(`Connected to MongoDB on ${mongoose.connection.db.databaseName}`);
+  app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+  });
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Error Code: ${err.code}, ${err.message}`);
 });
