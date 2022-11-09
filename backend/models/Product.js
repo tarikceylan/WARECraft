93cf0e1529be3e45760b96c3dd3abce4ead9a5af
tarikceylan <tarikceylan@outlook.com.tr>
2022-11-09 10:46:15 +0300

    Define Mongoose Schemas

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({});
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: [String],
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  productCode: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('Product', productSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      defaut: 'Employee',
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', userSchema);
